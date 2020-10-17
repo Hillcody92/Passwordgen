1ef689e0f0bed5e146e7448fadb35ade77e68756
Cody Hill <codyalexanderhill@gmail.com>
2020-10-16 20:38:52 -0500

    another commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var charset2 = "abcdefghijklmnopqrstuvwxyz0123456789";
-var charset3 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*/?";
-var charset4 = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*/?";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()-=+?.<>";
+var numbers = "0123456789";
 
 function writePassword() {
 
@@ -19,49 +19,50 @@ function writePassword() {
 
 function generatePassword() {
   var passwordLength = prompt("Length of Password?");
-  var passwordSpecial = confirm("Would you like your password to contain special characters? Okay for YES Cancel for'No'");
-  var passwordcase = confirm("Would you like the password to have capital letter? Okay for 'YES' Cancel for 'No'")
+  alert("Number of characters:" +passwordLength);
+  var passwordSpecial = confirm("Special characters? Okay for YES Cancel for No");
+  var passwordcase = confirm("Captial letters? Okay for Yes Cancel for No")
 
   if (passwordSpecial === true && passwordcase === true) {
     var length = passwordLength,
-      charset = charset3
+     uppercase = specialChar
     passwordText = "";
-    for (var i = 0, l = charset.length; i < length; ++i) {
-      passwordText += charset.charAt(Math.floor(Math.random() * l));
+    for (var i = 0, c = charset.length; i < length; ++i) {
+      passwordText += charset.charAt(Math.floor(Math.random() * c));
 
     }
     return passwordText;
   } 
   else if (passwordSpecial === false && passwordcase === true) {
     var length = passwordLength,
-      charset = charset2
+      numbers = uppercase
     passwordText = "";
-    for (var i = 0, l = charset.length; i < length; ++i) {
-      passwordText += charset.charAt(Math.floor(Math.random() * l));
+    for (var i = 0, c = charset.length; i < length; ++i) {
+      passwordText += charset.charAt(Math.floor(Math.random() * c));
 
     }
     return passwordText;
   }
   else if (passwordSpecial === false && passwordcase === false) {
     var length = passwordLength,
-      charset2 = charset4
+      lowercase = numbers
     passwordText = "";
-    for (var i = 0, l = charset.length; i < length; ++i) {
-      passwordText += charset.charAt(Math.floor(Math.random() * l));
+    for (var i = 0, c = charset.length; i < length; ++i) {
+      passwordText += charset.charAt(Math.floor(Math.random() * c));
     }
     return passwordText;
   }
   else if (passwordSpecial === true && passwordcase === false) {
     var length = passwordLength,
-      charset2 = charset3
+      specialChar = lowercase
     passwordText = "";
-    for (var i = 0, l = charset.length; i < length; ++i) {
-      passwordText += charset.charAt(Math.floor(Math.random() * l));
+    for (var i = 0, c = charset.length; i < length; ++i) {
+      passwordText += charset.charAt(Math.floor(Math.random() * c));
     }
     return passwordText;
   }
 }
-generatePassword()
+
 
 
 // Add event listener to generate button
